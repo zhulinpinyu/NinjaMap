456025bd8e6a30af7cc5de4ef526d06fbff7b88d
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-20 16:54:57 +0800

    basic google map work done

--- app/screens/home_screen.rb
+++ app/screens/home_screen.rb
@@ -1,11 +1,15 @@
 class HomeScreen < PM::Screen
   include YamlHelper
 
-  title "Your title here"
+  title "Google Map"
   stylesheet HomeScreenStylesheet
 
   def on_load
-    
+    GMSServices.provideAPIKey(key)
+    camera = GMSCameraPosition.cameraWithLatitude(22.53, longitude: 114.02, zoom: 14)
+    map_view = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+    map_view.myLocationEnabled = true
+    self.view = map_view
   end
 
   private

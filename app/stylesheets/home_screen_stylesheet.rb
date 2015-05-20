de231f9006e5b4ee837ef96ff1f312f2bf4db0b3
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-20 16:45:20 +0800

    clear code

--- app/stylesheets/home_screen_stylesheet.rb
+++ app/stylesheets/home_screen_stylesheet.rb
@@ -9,12 +9,4 @@ class HomeScreenStylesheet < ApplicationStylesheet
     st.background_color = color.white
   end
 
-  def hello_world(st)
-    st.frame = {t: 100, w: 200, h: 18, centered: :horizontal}
-    st.text_alignment = :center
-    st.color = color.battleship_gray
-    st.font = font.medium
-    st.text = 'Hello World'
-  end
-
 end

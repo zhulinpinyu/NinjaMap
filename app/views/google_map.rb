32c1b0038571818e5eb9e8245b492f547bdf1fe0
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-20 22:51:02 +0800

    refactor code

--- app/views/google_map.rb
+++ app/views/google_map.rb
@@ -6,10 +6,14 @@ class GoogleMap
   end
 
   def view
-    camera = GMSCameraPosition.cameraWithLatitude(22.53, longitude: 114.02, zoom: 14)
-    @map_view = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-    @map_view.myLocationEnabled = true
-    @map_view
+    coordinate = {latitude: 22.53, longitude: 114.02}
+    map_view = GMSMapView.mapWithFrame(CGRectZero, camera: camera(coordinate,14))
+    map_view.myLocationEnabled = true
+    map_view
+  end
+
+  def camera(coordinate,zoom)
+    GMSCameraPosition.cameraWithLatitude(coordinate[:latitude], longitude: coordinate[:longitude], zoom: zoom)
   end
 
   private

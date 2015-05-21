f4b49ee9d966668d495236b24d7ee59aeffc005f
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-21 12:03:27 +0800

    Refactor

--- app/views/map.rb
+++ app/views/map.rb
@@ -2,7 +2,19 @@ class Map
   attr_reader :map
 
   def initialize(options={type: "Google"})
-    @map = Object.const_get("#{options[:type]}").new
+    ##
+    # Not Work
+    ##
+    #@map = Kernel.const_get("Maps::#{options[:type]}").new
+    #@map = Object.const_get("Maps::#{options[:type]}").new
+
+    ##
+    # Work well (not Maps module in file)
+    ##
+    #@map = Kernel.const_get("#{options[:type]}").new
+    #@map = Object.const_get("#{options[:type]}").new
+
+    @map = clazz_instance(options[:type])
   end
 
   def view
@@ -12,4 +24,13 @@ class Map
   def center(coordinate={})
     map.center(coordinate)
   end
+
+  private
+
+    def clazz_instance(type)
+      case type
+      when "Google"
+        Maps::Google.new
+      end
+    end
 end
--- lib/maps/google.rb
+++ lib/maps/google.rb
@@ -1,24 +1,26 @@
-class Google
-  include YamlHelper
+module Maps
+  class Google
+    include YamlHelper
 
-  def initialize
-    GMSServices.provideAPIKey(key)
-  end
-
-  def view
-    coordinate = {latitude: 22.53, longitude: 114.02}
-    map_view = GMSMapView.mapWithFrame(CGRectZero, camera: center(coordinate))
-    map_view.myLocationEnabled = true
-    map_view
-  end
-
-  def center(coordinate)
-    GMSCameraPosition.cameraWithLatitude(coordinate[:latitude], longitude: coordinate[:longitude], zoom: 14)
-  end
-
-  private
-
-    def key
-      yaml["google_map"]
+    def initialize
+      GMSServices.provideAPIKey(key)
     end
+
+    def view
+      coordinate = {latitude: 22.53, longitude: 114.02}
+      map_view = GMSMapView.mapWithFrame(CGRectZero, camera: center(coordinate))
+      map_view.myLocationEnabled = true
+      map_view
+    end
+
+    def center(coordinate)
+      GMSCameraPosition.cameraWithLatitude(coordinate[:latitude], longitude: coordinate[:longitude], zoom: 14)
+    end
+
+    private
+
+      def key
+        yaml["google_map"]
+      end
+  end
 end

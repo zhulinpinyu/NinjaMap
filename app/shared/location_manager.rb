9190d06157170f927873fd044b71d4534575084c
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-21 22:35:39 +0800

    custom show current position

--- app/shared/location_manager.rb
+++ app/shared/location_manager.rb
@@ -4,13 +4,14 @@ module LocationManager
     @location_manager.delegate = self
     @location_manager.desiredAccuracy = KCLLocationAccuracyNearestTenMeters
     @location_manager.requestWhenInUseAuthorization
-    @location_manager.distanceFilter = 100
+    @location_manager.distanceFilter = 30
   end
 
   def locationManager(manager, didUpdateToLocation:newLocation, fromLocation:oldLocation)
     lat = newLocation.coordinate.latitude
     lon = newLocation.coordinate.longitude
-    map.marker({latitude: lat, longitude: lon})
-    #self.view.animateToCameraPosition(map.center({latitude: lat, longitude: lon}))
+    @marker ||= map.marker({latitude: lat, longitude: lon})
+    @marker.position = CLLocationCoordinate2DMake(lat,lon)
+    self.view.animateToCameraPosition(map.center({latitude: lat, longitude: lon}))
   end
 end
--- lib/maps/google.rb
+++ lib/maps/google.rb
@@ -10,19 +10,17 @@ module Maps
     def view
       coordinate = {latitude: 22.53, longitude: 114.02}
       @map_view = GMSMapView.mapWithFrame(CGRectZero, camera: center(coordinate))
-      @map_view.myLocationEnabled = false
-      @map_view.settings.myLocationButton = true
-      @map_view
     end
 
     def center(coordinate)
-      GMSCameraPosition.cameraWithLatitude(coordinate[:latitude], longitude: coordinate[:longitude], zoom: 14)
+      GMSCameraPosition.cameraWithLatitude(coordinate[:latitude], longitude: coordinate[:longitude], zoom: 16)
     end
 
     def marker(coordinate)
       marker = GMSMarker.alloc.init
       marker.position = CLLocationCoordinate2DMake(coordinate[:latitude], coordinate[:longitude])
       marker.map = map_view
+      marker
     end
 
     private

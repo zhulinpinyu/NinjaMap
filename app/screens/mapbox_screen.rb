74cce7b7a38641496a6a8396e4c44b3151aa12ce
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-22 00:44:33 +0800

    add menu

--- app/screens/mapbox_screen.rb
+++ app/screens/mapbox_screen.rb
@@ -5,11 +5,16 @@ class MapboxScreen < PM::Screen
   stylesheet MapboxScreenStylesheet
 
   def on_load
+    set_nav_bar_button :left, title: UIImage.imageNamed("list.png"), action: :show_menu, type: UIBarButtonItemStylePlain
     RMConfiguration.sharedInstance.setAccessToken(yaml[:mapbox])
     tile_source = RMMapboxSource.alloc.initWithMapID("mapbox.streets")
     map_view = RMMapView.alloc.initWithFrame(self.view.bounds, andTilesource: tile_source)
     map_view.zoom = 17
     map_view.centerCoordinate = CLLocationCoordinate2DMake(22.53,114.02)
     self.view = map_view
+  end
+
+  def show_menu
+    app_delegate.show_menu
   end
 end

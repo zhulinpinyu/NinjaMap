d6f624bd1ad1dada6d47f00bed03400dd4b1761f
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-21 15:25:28 +0800

    add myLocationButton

--- lib/maps/google.rb
+++ lib/maps/google.rb
@@ -10,6 +10,7 @@ module Maps
       coordinate = {latitude: 22.53, longitude: 114.02}
       map_view = GMSMapView.mapWithFrame(CGRectZero, camera: center(coordinate))
       map_view.myLocationEnabled = true
+      map_view.settings.myLocationButton = true
       map_view
     end
 

672078363ca01818358cedf2346a2671a760222e
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-26 17:23:48 +0800

    refactor code

--- app/shared/location_manager.rb
+++ app/shared/location_manager.rb
@@ -10,16 +10,14 @@ module LocationManager
   def locationManager(manager, didUpdateToLocation:newLocation, fromLocation:oldLocation)
     lat = newLocation.coordinate.latitude
     lon = newLocation.coordinate.longitude
+    @coordinate = {latitude: lat, longitude: lon}
     if self.class.to_s == "HomeScreen"
       @coordinate = CoordinateTransform.wgs2gcj(lat,lon)
       @marker ||= map.marker(@coordinate)
       @marker.position = CLLocationCoordinate2DMake(@coordinate[:latitude],@coordinate[:longitude])
       self.view.animateToCameraPosition(map.center(@coordinate))
     elsif self.class.to_s == "MapboxScreen"
-      @coordinate = {latitude: lat, longitude: lon}
       self.view.centerCoordinate = CLLocationCoordinate2DMake(lat,lon)
-    elsif self.class.to_s == "AmapScreen"
-      @coordinate = {latitude: lat, longitude: lon}
     end
   end
 end

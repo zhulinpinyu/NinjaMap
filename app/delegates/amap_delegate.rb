3d755c09c9a26773399de6c4cc223b0c86e21a10
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-29 17:37:04 +0800

    add select/unselect style

--- app/delegates/amap_delegate.rb
+++ app/delegates/amap_delegate.rb
@@ -1,5 +1,5 @@
 class AmapDelegate
-  def mapView(map_view,viewForAnnotation: annotation)
+  def mapView(map_view, viewForAnnotation: annotation)
     if annotation.is_a?(MAPointAnnotation)
       @annotation_view_id ||= "pointReuseIndetifier"
       annotation_view = map_view.dequeueReusableAnnotationViewWithIdentifier(@annotation_view_id)
@@ -10,6 +10,18 @@ class AmapDelegate
       #annotation_view.animatesDrop = true         #设置标注动画显示，默认为NO
       # #annotation_view.draggable = true            #设置标注可以拖动，默认为NO
       annotation_view.setPinColor(MAPinAnnotationColorRed)
+    end
+  end
+
+  def mapView(map_view, didSelectAnnotationView: view)
+    if view.is_a?(MAPinAnnotationView)
+      view.setPinColor(MAPinAnnotationColorGreen)
+    end
+  end
+
+  def mapView(map_view, didDeselectAnnotationView: view)
+    if view.is_a?(MAPinAnnotationView)
+      view.setPinColor(MAPinAnnotationColorRed)
     end
   end
 end
--- lib/maps/amap.rb
+++ lib/maps/amap.rb
@@ -11,11 +11,11 @@ module Maps
       coordinate = {latitude: 22.53, longitude: 114.02}
       @map_view = MAMapView.alloc.initWithFrame([[0,0],[200,200]])
       @map_view.setCenterCoordinate(coordinate2D(coordinate))
-      @map_view.setZoomLevel(17, animated: false)
+      @map_view.setZoomLevel(1, animated: false)
       @map_view.showsScale = false
       @map_view.compassOrigin = CGPointMake(@map_view.compassOrigin.x, 22)
       @map_view.showsUserLocation = true
-      @map_view.setUserTrackingMode(MAUserTrackingModeFollow,animated: true)
+      @map_view.setUserTrackingMode(MAUserTrackingModeNone,animated: true)
       @map_view.delegate = amap_delegate
       @map_view
     end
